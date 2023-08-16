9acbeec9f21b1a48a4a324d70992ffd6e2852607
Muas09 <116275467+Muas09@users.noreply.github.com>
2023-08-16 07:31:11 +0700

    Update style in user_component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NotFound_component from "./components/NotFound_component";
 const App = () => {
   const [users, setUsers] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [searched, setSearched] = useState(false); // Thêm state để theo dõi việc đã thực hiện tìm kiếm hay chưa
 
   const handleAddUser = (newUser) => {
     setUsers([...users, newUser]);
@@ -19,20 +20,21 @@ const App = () => {
 
   const handleSearch = (value) => {
     setSearchTerm(value);
+    setSearched(true); // Đã thực hiện tìm kiếm
   };
 
   return (
     <Router>
       <div className="container">
         <div className="titleCard">LIST SOCIAL CARD</div>
-        <SearchAdd_component addUser={handleAddUser} />
+        <SearchAdd_component addUser={handleAddUser} onSearch={handleSearch} />
         <div className="postUser">
-          {filteredUsers.length > 0 ? (
+          {searched && filteredUsers.length === 0 ? (
+            <NotFound_component />
+          ) : (
             filteredUsers.map((user, index) => (
               <User_component key={index} user={user} />
             ))
-          ) : (
-            <NotFound_component />
           )}
         </div>
       </div>
--- src/components/User_component/index.jsx
+++ src/components/User_component/index.jsx
@@ -1,29 +1,53 @@
 import React from "react";
+import styles from "./style.module.css";
 
 const UserComponent = ({ user }) => {
   return (
-    <div className="post">
-      <div className="UserTitle">
-        <div className="AvatarUser">
-          <img
-            className="avatar"
-            src={user.avatar}
-            alt={`Avatar of ${user.name}`}
-          />
-          <div className="UserName">
-            <div className="username">{user.name}</div>
-            <div className="datetime">{user.datetime}</div>
+    <div className={styles.post}>
+      <div className={styles.UserTitle}>
+        <img
+          className={styles.avatar}
+          src="Assets/Avatar_PhuNguyen.svg"
+          alt="Avatar 1"
+        />
+        <div className={styles.UserName}>
+          <div className={styles.username}>Phu Nguyen</div>
+          <div className={styles.datetime}>14/08/2023</div>
+        </div>
+        <div className={styles.IconEditDelete}>
+          <img className={styles.IconEdit} src="Assets/Icon_Edit.svg" alt="" />
+          <img className={styles.IconDelete} src="Assets/Icon_Delete.svg" alt="" />
+
+        </div>
+        <div className={styles.content}>
+          It is a long established fact that a reader will be distracted by
+          the readable content of a page when looking at its layout.{" "}
+        </div>
+        <img className={styles.image} src="Assets/Image_Cat.svg" alt="Image 1" />
+      </div>
+{/* ---------------------------------------------------- */}
+      <div className={styles.post}>
+        <div className={styles.UserTitle}>
+          <div className={styles.AvatarUser}>
+            <img
+              className={styles.avatar}
+              src={user.avatar}
+              alt={`Avatar of ${user.name}`}
+            />
+            <div className={styles.UserName}>
+              <div className={styles.username}>{user.name}</div>
+              <div className={styles.datetime}>{user.datetime}</div>
+            </div>
+          </div>
+          <div className={styles.IconEditDelete}>
+            <img className={styles.IconEdit} src="Assets/Icon_Edit.svg" alt="" />
+            <img className={styles.IconDelete} src="Assets/Icon_Delete.svg" alt="" />
           </div>
         </div>
-        <div className="IconEditDelete">
-          <img className="IconEdit" src="Assets/Icon_Edit.svg" alt="" />
-          <img className="IconDelete" src="Assets/Icon_Delete.svg" alt="" />
-        </div>
-      </div>
-      <div className="content">{user.content}</div>
-      <img className="image" src={user.image} alt={`Image related to ${user.name}`} />
-    </div>
-  );
+        <div className={styles.content}>{user.content}</div>
+        <img className={styles.image} src={user.image} alt={`Image related to ${user.name}`} />
+      </div> </div>
+      );
 };
 
-export default UserComponent;
+      export default UserComponent;
