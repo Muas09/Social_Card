f7b6cb344e9a9ed9714a2d01191ceedbc69cb1b3
Muas09 <116275467+Muas09@users.noreply.github.com>
2023-08-18 07:03:58 +0700

    fix file upload input  in ModalAdd folder

--- src/components/ModalAdd/index.jsx
+++ src/components/ModalAdd/index.jsx
@@ -1,7 +1,31 @@
+
 import React from "react";
 import styles from "./style.module.css";
 
+const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/lagos/image/upload';
+const CLOUDINARY_UPLOAD_PRESET = 'moox1jnq';
+
 const AddCard = () => {
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+
+    fetch(CLOUDINARY_URL, {
+      method: 'POST',
+      body: formData,
+    })
+      .then(response => response.json())
+      .then((data) => {
+        if (data.secure_url !== '') {
+          const uploadedFileUrl = data.secure_url;
+          localStorage.setItem('passportUrl', uploadedFileUrl);
+        }
+      })
+      .catch(err => console.error(err));
+  };
+
   return (
     <div className={styles.newCard}>
       <div className={styles.modelAdd}>
@@ -10,33 +34,41 @@ const AddCard = () => {
             <div className={styles.modalHeader}>Add new card</div>
             <div className={styles.modalBody}>
               <div className={styles.cardText}>
-                <li>Avatar</li>
-                <li>Name</li>
-                <li>Decription</li>
-                <li>Image</li>
+                <ul>
+                  <li>Avatar</li>
+                  <li>Name</li>
+                  <li>Description</li>
+                  <li>Image</li>
+                </ul>
               </div>
               <div className={styles.cardInput}>
                 <div className={styles.contentAvatar}>
-                  <input className={styles.updateImage} type="file" id="fileupload" placeholder="Upload image"></input>
-                  {/* <img src="Assets/upload_Files.svg" alt="" />
-                  <div className={styles.decription}>Upload image</div> */}
+                  <label htmlFor="fileupload"></label>
+                  <input
+                    className={styles.updateImage}
+                    type="file"
+                    id="fileupload"
+                    onChange={handleImageUpload}
+                  />
                 </div>
                 <div className={styles.cardInput}>
-                  <input type="text" />
+                  <input type="text" placeholder="Name" />
                 </div>
-
                 <div className={styles.cardInput}>
-                  <textarea></textarea>
+                  <textarea placeholder="Description"></textarea>
                 </div>
                 <div className={styles.contentAvatar}>
-                <input className={styles.updateImage} type="file" id="fileupload"></input> 
-                  {/* <img src="Assets/upload_Files.svg" alt="" />
-                  <div className={styles.decription}>Upload image</div> */}
+                  <label htmlFor="fileupload"></label>
+                  <input
+                    className={styles.updateImage}
+                    type="file"
+                    id="fileupload"
+                    onChange={handleImageUpload}
+                  />
                 </div>
               </div>
             </div>
           </div>
-
           <div className={styles.btnClose}>
             <div className={styles.btnSave}>Save</div>
             <div className={styles.btnCancel}>Cancel</div>
